23c1526823596c2ef9a81813da4024fa32607f23
Aniket10-IN <aniketsharma7515@gmail.com>
2023-07-28 14:02:03 +0530

    setup and data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import pandas as pd
+import logging
+
+
+
+from src.exception import CustomException
+from src.logger import logging
+
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    raw_data_path = os.path.join('artifacts', 'data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('Data Ingestion has started')
+        try:
+            df = pd.read_csv('dataset/clean_data.csv')
+            logging.info('Reading data')
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok= True)
+            df.to_csv(self.ingestion_config.raw_data_path, header= True, index = False )
+
+
+            logging.info('data ingestion completed')
+
+            return (self.ingestion_config.raw_data_path)
+
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
+
